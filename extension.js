1a1378e607c719e19359ac1348baef01e1031c29
h4bUb <habub.games@gmail.com>
2015-05-12 19:21:59 +0500

    Update extension.js

--- extension.js
+++ extension.js
@@ -114,6 +114,6 @@
     }));
 
     //Start the bot and extend it when it has loaded.
-    $.getScript("https://rawgit.com/Yemasthui/basicBot/master/basicBot.js", extend);
+    $.getScript("https://rawgit.com/768loved/basicBot/master/basicBot.js", extend);
 
 }).call(this);
